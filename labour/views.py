6a26caebb585ed14a8a9e94c75eaa8015054ec1a
zhangbo <zhangbo@cfpamf.org.cn>
2015-08-09 22:46:19 +0800

    update

--- labour/views.py
+++ labour/views.py
@@ -691,11 +691,7 @@ def labour_import(request, form_class=LabourImportForm, template_name='labour/la
                     serial_id += 1
                     if request.user.account.level in (0, 1):
                         employee.is_active = 1
-                    try:
                         employee.save()
-                    except pytz.NonExistentTimeError:
-
-
 
                     contract = Contract(
                         employee=employee, job_type=format_value(line[19]), company_protocal_start=format_date(line[20]), company_protocal_end=format_date(line[21]),
